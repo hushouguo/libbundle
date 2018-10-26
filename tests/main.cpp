30865c0bb9863b73edf2b0e5be8aa755997e0b0e
hushouguo <kilimajaro@gmail.com>
2018-10-27 00:06:44 +0800

    fix bug

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,11 +14,12 @@
 #include "recordclient/test_recordclient.h"
 #include "entity/test_entity.h"
 #include "lockfree/test_lockfree.h"
+#include "uv/test_uv.h"
 
 int main(int argc, char** argv) {	
 	if (!bundle::init_runtime_environment(argc, argv)) { return 1; }
 
-	test_tools();
+	//test_tools();
 	//test_net();
 	//test_csv("./csv/test.csv");
 	//test_xml("./xml/conf.xml");
@@ -28,6 +29,7 @@ int main(int argc, char** argv) {
 	//test_recordclient("127.0.0.1", 9990);
 	//test_entity();
 	//test_lockfree();
+	test_uv();
 
 	bundle::shutdown_bundle_library();
 	return 0;
